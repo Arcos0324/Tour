f615343c9aa9034f0e391729794482f8893c3055
Arcos <scoorkchepe@gmail.com>
2019-10-30 10:45:22 -0600

    Para hostear

--- vr/components/Shape.js
+++ vr/components/Shape.js
@@ -10,7 +10,8 @@ export default class Shape extends React.Component {
         let colors = ['#C33000', '#1FA001', '#214EB4', '#D3B010'];
         //let textures = ['../../static_assets/Texturas/Black.jpg', '../../static_assets/Texturas/Bricks.jpg', '../../static_assets/Texturas/Wall.jpg', '../../static_assets/Texturas/Ground.jpg']
         return(
-            <Component /*texture={textures[this.props.colorNum]}*/ style={{
+            <Component //texture={textures[this.props.colorNum]} 
+            style={{
                 color: colors[this.props.colorNum],
                 transform: this.props.transform,
             }}/>
